ae8649e067c6be2b9de137762bd5574a9a4e164d
mikegyamfi <codeage20@gmail.com>
2023-06-20 09:36:12 +0000

    update sms

--- intel_app/views.py
+++ intel_app/views.py
@@ -84,7 +84,7 @@ def airtel_tigo(request):
                 print(response.text)
 
                 sms_body = {
-                    'recipient': f"0{request.user.phone}",
+                    'recipient': f"233{request.user.phone}",
                     'sender_id': 'Noble Data',
                     'message': sms_message
                 }
@@ -101,7 +101,7 @@ def airtel_tigo(request):
                 sms_message = f"Hello @{request.user.username}. Something went wrong with your transaction. Contact us for enquiries.\nBundle: {bundle}MB\nPhone Number: {phone_number}.\nReference: {payment_reference}\nThank you for using Noble Data GH.\n\nThe Noble Data GH"
 
                 sms_body = {
-                    'recipient': f"0{request.user.phone}",
+                    'recipient': f"233{request.user.phone}",
                     'sender_id': 'Noble Data',
                     'message': sms_message
                 }
@@ -118,7 +118,7 @@ def airtel_tigo(request):
             sms_message = f"Hello @{request.user.username}. Something went wrong with your transaction. Contact us for enquiries.\nBundle: {bundle}MB\nPhone Number: {phone_number}.\nReference: {payment_reference}\nThank you for using Noble Data GH.\n\nThe Noble Data GH"
 
             sms_body = {
-                'recipient': f'0{request.user.phone}',
+                'recipient': f'233{request.user.phone}',
                 'sender_id': 'Noble Data',
                 'message': sms_message
             }

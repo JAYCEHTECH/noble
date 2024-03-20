7e92109fb062dd5cc8e49e308de633acacb7dca7
mikegyamfi <codeage20@gmail.com>
2024-03-20 22:01:31 +0000

    debug

--- intel_app/views.py
+++ intel_app/views.py
@@ -1158,7 +1158,7 @@ def request_successful(request, reference):
 
 def topup_list(request):
     if request.user.is_superuser:
-        topup_requests = models.TopUpRequest.objects.all().order_by('date').reverse()
+        topup_requests = models.TopUpRequest.objects.filter(status=False).order_by('date').reverse()
         context = {
             'requests': topup_requests,
         }
